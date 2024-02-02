aa32cee6f4ba34c287ff594f79ee4e72c241c6e3
mitch <mitch@debian.swag.local>
2024-02-01 21:03:41 -0800

    env vars

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
+use clap::Parser;
 use serde_json::Value;
-use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::Write;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::process::Command;
-use std::process::Stdio;
+use std::io::{BufRead, BufReader, Read, Result, Write};
+use std::net::{TcpListener, TcpStream};
+use std::ops::Add;
+use std::process::{Command, Stdio};
 use std::thread;
 
+#[derive(Parser)]
+struct Cli {
+    ip_addr: String,
+    port: u16,
+}
 fn main() {
-    let listener = start_server("0.0.0.0", 8080);
+    let args = Cli::parse();
+    let listener = start_server(&args.ip_addr, args.port).expect(&format!(
+        "Failed to start server on {}:{}",
+        &args.ip_addr, args.port
+    ));
+    println!("Server started on {}:{}", &args.ip_addr, args.port);
     for tcp_stream in listener.incoming() {
         match tcp_stream {
             Ok(stream) => {
                 println!("New connection from {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    handle_connection(stream.try_clone().expect("couldn't clone stream"))
-                });
+                thread::spawn(move || handle_connection(stream));
             }
             Err(e) => println!("Error: {}", e),
         }
     }
-
-    //execute_command("traceroute", &["8.8.8.8"]).unwrap();
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(stream: TcpStream) -> Result<()> {
     let mut reader = BufReader::new(&stream);
 
     let mut cmd_buf = String::new();
@@ -36,49 +39,68 @@ fn handle_connection(stream: TcpStream) {
     let json_obj = deserialize(cmd_buf);
     let cmd = json_obj["cmd"].as_str().unwrap();
     let args = json_obj["args"].as_str().unwrap();
+    //let env_vars = json_obj["env"].as_str().unwrap();
     let splitted_args: Vec<&str> = args.split(' ').collect();
     let slice_ref: &[&str] = &splitted_args;
 
-    execute_command(cmd, slice_ref, stream)
+    let env_vars: Option<Vec<(&str, &str)>> = Some(vec![("NAME", "grok")]);
+    execute_command(cmd, slice_ref, &stream, env_vars)
 }
 
 fn deserialize(data: String) -> Value {
+    println!("{}", &data);
     serde_json::from_str(&data).expect("couldn't deserialize")
 }
 
-fn start_server(ip_addr: &str, port: u16) -> TcpListener {
+fn start_server(ip_addr: &str, port: u16) -> Result<TcpListener> {
     let addr = format!("{}:{}", ip_addr, port);
-    let listener = TcpListener::bind(&addr);
-    match listener {
-        Ok(listener) => {
-            println!("Server listening on {}", &addr);
-            listener
-        }
-        Err(e) => panic!("Cannot bind to {}, \n{}", &addr, e),
-    }
+    TcpListener::bind(&addr)
 }
 
-fn execute_command(cmd_str: &str, arg_str: &[&str], mut stream: TcpStream) {
-    let mut child = Command::new(cmd_str)
-        .args(arg_str)
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to execute command");
+fn execute_command(
+    cmd_str: &str,
+    arg_str: &[&str],
+    mut stream: &TcpStream,
+    env_vars: Option<Vec<(&str, &str)>>,
+) -> Result<()> {
+    let command_line = format!("{} {}", cmd_str, arg_str.join(" "));
 
-    let stdout = child.stdout.take().expect("Failed to open stdout");
-    let reader = io::BufReader::new(stdout);
+    let mut command = Command::new("sh");
+    command.arg("-c").arg(&command_line);
 
-    for line in reader.lines() {
-        match line {
-            Ok(line) => {
-                let line = format!("{}\n", line);
-
-                stream
-                    .write_all(line.as_bytes())
-                    .expect("Could not write to stream")
-            }
-            Err(e) => eprintln!("Error: {}", e),
+    if let Some(vars) = env_vars {
+        for (key, value) in vars {
+            command.env(key, value);
         }
     }
-    let _ = child.wait().unwrap();
+
+    let mut child = command
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn();
+
+    let mut child = match child {
+        Ok(child) => child,
+        Err(e) => {
+            let line = format!("Failed to execute command: {}", e);
+            stream.write_all(line.as_bytes());
+            return Err(e.into());
+        }
+    };
+    let stdout = child.stdout.take().expect("Failed to open stdout");
+    let stderr = child.stderr.take().expect("Failed to open stderr");
+    stream_write_lines(stream, BufReader::new(stdout));
+    stream_write_lines(stream, BufReader::new(stderr));
+
+    // Wait for child process to complete
+    child.wait()?;
+    Ok(())
+}
+
+fn stream_write_lines<R: Read>(mut stream: &TcpStream, reader: BufReader<R>) -> Result<()> {
+    for line in reader.lines() {
+        let line = line?.add("\n");
+        stream.write_all(line.as_bytes())?;
+    }
+    Ok(())
 }
